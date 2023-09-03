e57c25c0e9a78fd52a6641707a6782ccabc2bb52
LefterisIkaria <lolopap21@gmail.com>
2023-09-03 17:21:44 +0300

    added color to cosnole logs for better readability

--- src/config/logger_config.py
+++ src/config/logger_config.py
@@ -1,6 +1,29 @@
-import os, json, sys
+import os, json
 import logging
 import logging.config
+
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        'TIMESTAMP': '\033[90m',
+        'LEVEL_NAME': {
+            'WARNING': '\033[93m',
+            'INFO': '\033[92m',
+            'DEBUG': '\033[94m',
+            'CRITICAL': '\033[91m',
+            'ERROR': '\033[91m'
+        },
+        'MESSAGE': '\033[97m',
+        'ENDC': '\033[0m'
+    }
+
+    def format(self, record):
+        log_message = super(ColoredFormatter, self).format(record)
+        return (
+            f"{self.COLORS['TIMESTAMP']}{self.formatTime(record, self.datefmt)}{self.COLORS['ENDC']} "
+            f"{self.COLORS['LEVEL_NAME'].get(record.levelname)}{record.levelname}{self.COLORS['ENDC']} "
+            f"{self.COLORS['MESSAGE']}{record.getMessage()}{self.COLORS['ENDC']}"
+        )
+
 
 def setup_logging_from_json(filepath: str):
 
@@ -17,3 +40,14 @@ def setup_logging_from_json(filepath: str):
             handler['filename'] = handler['filename'].format(USER_HOME=log_dir)
 
     logging.config.dictConfig(config)
+
+     # Create a ColoredFormatter
+    colored_formatter = ColoredFormatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    # Get the root logger
+    logger = logging.getLogger()
+
+    # Update the console handler to use the ColoredFormatter
+    for handler in logger.handlers:
+        if isinstance(handler, logging.StreamHandler):
+            handler.setFormatter(colored_formatter)
