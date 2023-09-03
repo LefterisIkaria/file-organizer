64ee6fa569cbbde2b4dbfbe6d3d7c3f369bdc681
LefterisIkaria <lolopap21@gmail.com>
2023-09-03 16:57:17 +0300

    added app.json in {USER_HOME}/.file-organizer and configured logging

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 import argparse
-from gui.app import App
-from src.config.logger_config import setup_file_logging
+import logging
+import os
+import shutil
+from src.gui.app import App
+from src.config.logger_config import setup_logging_from_json
 from src.organizer.file_organizer import FileOrganizer
 from src.utils.reset_directory import reset_directory
+
+logger = logging.getLogger(__name__)
 
 def parse_args():
     parser = argparse.ArgumentParser(description="File Organizer Application")
@@ -12,9 +17,27 @@ def parse_args():
     parser.add_argument("--directory", type=str, help="Directory to reset")
     return parser.parse_args()
 
-def main():
-    logger = setup_file_logging()
+# def setup():
+#     user_home = os.path.expanduser("~")
+#     app_dir = os.path.join(user_home, ".file-organizer")
+#     config_path = os.path.join(app_dir, "app.json")
+#     template_path = "config/app.template.json"
 
+#     # Create the directory if it doesn't exist
+#     if not os.path.exists(app_dir):
+#         os.makedirs(app_dir)
+
+#     # Check if app.json exists
+#     if not os.path.exists(config_path):
+#         # Copy app.template.json to app.json
+#         shutil.copy(template_path, config_path)
+
+def get_config_path():
+    home_path = os.path.expanduser("~")
+    return os.path.join(home_path, ".file-organizer", "app.json")
+
+def main():
+    setup_logging_from_json("config/logging.json")
     args = parse_args()
 
     if args.gui:
@@ -23,8 +46,9 @@ def main():
         app.mainloop()
     elif args.organize:
         logger.info("Running the file organizer script...")
+        app_config_path = get_config_path()
         organizer = FileOrganizer()
-        organizer.run("config/config.json")
+        organizer.run(app_config_path)
     elif args.reset:
         if args.directory:
             logger.info(f"Running the reset directory script on {args.directory}...")
